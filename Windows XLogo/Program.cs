51470909f637410b718fc0af45a77916ec8a4193
chugga_fan <chuggafans@gmail.com>
2016-09-12 18:42:57 -0400

    Currently working on creating a windows sample but am having trouble compiling the entire solution at once currently

---
+++ Windows XLogo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows_XLogo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var sample = new Sample())
+            {
+                sample.Show();
+                System.Threading.Thread.Sleep(10000);
+            }
+        }
+    }
+}
